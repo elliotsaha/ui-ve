174ca13bf860124575433a38a83e1512f23d14b6
Heril Saha <herilsaha06@gmail.com>
2023-07-09 01:06:05 -0600

    build(vanilla-extract): replaced styled-components with vanilla extract package

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "jest";
 
 const config: Config = {
+  transform: {
+    "\\.styles\\.ts$": "@vanilla-extract/jest-transform",
+  },
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/"],
   preset: "ts-jest",
   verbose: true,
@@ -8,6 +11,7 @@ const config: Config = {
   collectCoverageFrom: [
     "<rootDir>/src/components/**/*.{js,jsx,ts,tsx}",
     "!<rootDir>/src/components/**/*.stories.{js,jsx,ts,tsx}",
+    "!<rootDir>/src/components/**/index.{js,jsx,ts,tsx}",
   ],
   coverageDirectory: "<rootDir>/coverage/jest",
   testEnvironment: "jest-environment-jsdom",
--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,7 @@ import external from "rollup-plugin-peer-deps-external";
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import dts from "rollup-plugin-dts";
-
-const packageJson = require("./package.json");
+import packageJson from "./package.json";
 
 const rollupConfig = [
   {
